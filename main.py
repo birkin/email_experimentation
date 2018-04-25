91427c3e4560bfd17c0621431ee023f9c2dbaf8d
birkin <birkin.diana@gmail.com>
2018-04-25 17:02:07 -0400

    sets imports.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import requests
+import email, imaplib, os, re, subprocess, sys
 
-response = requests.get('https://httpbin.org/ip')
-
-print('Your IP is {0}'.format(response.json()['origin']))
+print( 'hello world' )
