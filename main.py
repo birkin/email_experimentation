95340f13b821008d0f0ae4d5cc25e72b348700ca
birkin <birkin.diana@gmail.com>
2018-04-26 11:00:36 -0400

    adds logging.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import email, imaplib, os, re, subprocess, sys
+import email, imaplib, logging, os, re, subprocess, sys
 import rfc822py3
+
+
+logging.basicConfig(
+    filename=os.environ['EMLEXP__LOG_PATH'],
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S',
+    )
+log = logging.getLogger(__name__)
+log.debug( 'starting log' )
+
+
+MAIL_DOMAIN = os.environ['EMLEXP__MAIL_DOMAIN']
+
 
 print( 'hello world' )
